6062ef7ac35c48bbb8b3883c67450bbb667554bb
katherine vanuum <kvanuum@gmail.com>
2019-05-19 13:34:34 -0500

    add db config string

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
+#app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogzz:beproductive@localhost:8889/blogzz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
+
 
 class Blog(db.Model):
 
