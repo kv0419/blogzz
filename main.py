2222d9321e501daeb9ca0b56190514c62ffcb7ee
katherine vanuum <kvanuum@gmail.com>
2019-05-19 23:07:04 -0500

    so many errors to manage!

--- main.py
+++ main.py
@@ -14,12 +14,76 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(10000))
-    
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
+
+class User(db.Model):
+
+        id = db.Column(db.Integer,primary_key = True)        
+        username = db.Column(db.String(120),unique = True)
+        password = db.Column(db.String(200))
+        blogs = db.relationship('Blog',backref = 'owner')
+
+        def __init__(self, username, password, blogs):
+            self.username = username
+            self.password = password
+            self.blogs = blogs
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login','signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash('Logged In')
+            return redirect('/newpost')
+        else:
+            flash('Password incorrect or Username does not exist','error')
+    return render_template('login.html')
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
         
+        # if verify != password:
+        #     verify_error = "these passwords do not match"
+        #     return render_template(signup.html, verify_error = verify_error)
+        # 
+        # else:
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User (username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = Username
+            return redirect ('/newpost')
+        else:
+            ## some responses here
+            return "<h1> This User already exists, try another!<h1>"
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
 
 @app.route('/blogview', methods = ['GET',"POST"])
 def blogview():
@@ -41,6 +105,7 @@ def newpost():
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
+        blog_owner = User.query.filter_by()
 
         title_error =''
         if (blog_title ==''):
@@ -54,8 +119,8 @@ def newpost():
             return render_template("newblog.html", title_error = title_error, body_error = body_error)
         
         else:
-        
-            new_blog = Blog(blog_title, blog_body)
+                   
+            new_blog = Blog(blog_title, blog_body, blog_owner)
             db.session.add(new_blog)
             db.session.commit()
             strid = str(new_blog.id)
@@ -66,8 +131,18 @@ def newpost():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('/login.html')
+    owner = User.query.filter_by(username=session['username']).first()
 
+    if request.method == 'POST':
+        blog_title = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_title, blog_body, owner)
+        
+        db.session.add(new_blog)
+        db.session.commit()
+    blogs = Blog.query.filter_by(owner = owner).all()
+    
+    return render_template('allblogs.html', stuff = Blog.query.all())
 
 
 if __name__ == '__main__':
